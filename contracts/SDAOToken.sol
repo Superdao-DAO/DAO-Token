a4b8ea77c5b23fbe1c88e27c17130102f0f9d1e8
Pavel Rubin <pash7ka@gmail.com>
2020-01-26 00:28:24 +0300

    First version

---
+++ contracts/SDAOToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+import '@openzeppelin/contracts/token/ERC777/ERC777.sol';
+
+contract SDAOToken is ERC777 {
+    string constant NAME    = 'SuperDAO Token';
+    string constant SYMBOL  = 'SDAO';
+    uint256 constant TOTAL_SUPPLY = 100_000_000 * 10**18;
+
+    constructor() ERC777(NAME, SYMBOL, new address[](0)) public {
+        _mint(msg.sender, msg.sender, TOTAL_SUPPLY, '', '');
+    }
+}
+
